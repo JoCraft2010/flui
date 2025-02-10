e7ca28db9ceac76b63bfd35c90fa9ed301257722
Johannes Preiser <jocraft2010@gmail.com>
2025-02-10 16:33:06 +0100

    Reorganize output handling code

---
+++ flui/output.h
@@ -0,0 +1,39 @@
+#include <assert.h>
+#include <getopt.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <wayland-server-core.h>
+#include <wlr/backend.h>
+#include <wlr/render/allocator.h>
+#include <wlr/render/wlr_renderer.h>
+#include <wlr/types/wlr_cursor.h>
+#include <wlr/types/wlr_compositor.h>
+#include <wlr/types/wlr_data_device.h>
+#include <wlr/types/wlr_input_device.h>
+#include <wlr/types/wlr_keyboard.h>
+#include <wlr/types/wlr_output.h>
+#include <wlr/types/wlr_output_layout.h>
+#include <wlr/types/wlr_pointer.h>
+#include <wlr/types/wlr_scene.h>
+#include <wlr/types/wlr_seat.h>
+#include <wlr/types/wlr_subcompositor.h>
+#include <wlr/types/wlr_xcursor_manager.h>
+#include <wlr/types/wlr_xdg_shell.h>
+#include <wlr/util/log.h>
+#include <xkbcommon/xkbcommon.h>
+
+#ifndef __flui_output_h
+#define __flui_output_h
+
+struct flui_output {
+	struct wl_list link;
+	struct flui_server *server;
+	struct wlr_output *wlr_output;
+	struct wl_listener frame;
+	struct wl_listener request_state;
+	struct wl_listener destroy;
+};
+
+void server_new_output(struct wl_listener *listener, void *data);
+
+#endif
